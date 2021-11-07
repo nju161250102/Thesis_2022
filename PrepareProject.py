3119800567742ca2bc91d841ec48b5955e3cddc2
nju161250102 <161250102@smail.nju.edu.cn>
2021-11-07 15:21:52 +0800

    Add: 根据时间段筛选版本

--- PrepareProject.py
+++ PrepareProject.py
@@ -1,6 +1,7 @@
 """
 项目下载与扫描脚本
 """
+from datetime import datetime
 import json
 import sys
 
@@ -20,6 +21,19 @@ def collect():
         # 如果指定了筛选的版本，合并
         if "select" in config.keys() and len(config["select"]) > 0:
             info["select"] = config["select"]
+        # 如果指定了保留的版本的时间范围，筛选
+        select_versions = info["versions"][:]
+        if "startDate" in config.keys() and config["startDate"] is not None:
+            start_date = datetime.strptime(config["startDate"], "%Y-%m-%d")
+            select_versions = list(filter(
+                lambda v: datetime.strptime(v["updateTime"], "%Y-%m-%d %H:%M:%S") >= start_date, select_versions))
+        if "endDate" in config.keys() and config["endDate"] is not None:
+            end_date = datetime.strptime(config["endDate"], "%Y-%m-%d")
+            select_versions = list(filter(
+                lambda v: datetime.strptime(v["updateTime"], "%Y-%m-%d %H:%M:%S") <= end_date, select_versions))
+        # 将符合条件的版本号保存
+        if len(select_versions) != len(info["versions"]):
+            info["select"] = list(map(lambda v: v["number"], select_versions))
         result[p] = info
     # 保存json到数据目录
     with open(PathUtils.join_path("project.json"), "w") as f:
